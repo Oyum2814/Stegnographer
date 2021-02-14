9720507228fdbb927f4c842a970181cf8e727c4a
Om <ommukherjee28@gmail.com>
2021-02-14 12:24:01 +0530

    demo

--- stegno.py
+++ stegno.py
@@ -169,8 +169,8 @@ def encode_start_window():
     dat = EntryWithPlaceholder(newWindow,"Or encode a text")
     dat.place(x=20, y=300)
 
-    button_start_encoding = Button(newWindow, height=3, width=50, text="ENCODE THE TEXT INSIDE",transition=None, bg="grey", command=encode_text_start, font=('Helvetica', 12, 'bold'))
-    button_start_encoding.place(x=20, y=350)
+    button_start_encoding = Button(newWindow, height=3, width=10, text="ENCODE",transition=None, bg="grey", font=('Helvetica', 12, 'bold'))
+    button_start_encoding.place(x=250, y=290)
 
 
 
@@ -223,124 +223,6 @@ def decode_start():
 
 
 #########text########
-def encode_text_start():
-    encode_txt()
-def genData(data):
-
-        # list of binary codes
-        # of given data
-        newd = []
-
-        for i in data:
-            newd.append(format(ord(i), '08b'))
-        return newd
-
-# Pixels are modified according to the
-# 8-bit binary data and finally returned
-def modPix(pix, data):
-
-    datalist = genData(data)
-    lendata = len(datalist)
-    imdata = iter(pix)
-
-    for i in range(lendata):
-
-        # Extracting 3 pixels at a time
-        pix = [value for value in imdata.__next__()[:3] +
-                                imdata.__next__()[:3] +
-                                imdata.__next__()[:3]]
-
-        # Pixel value should be made
-        # odd for 1 and even for 0
-        for j in range(0, 8):
-            if (datalist[i][j] == '0' and pix[j]% 2 != 0):
-                pix[j] -= 1
-
-            elif (datalist[i][j] == '1' and pix[j] % 2 == 0):
-                if(pix[j] != 0):
-                    pix[j] -= 1
-                else:
-                    pix[j] += 1
-                # pix[j] -= 1
-
-        # Eighth pixel of every set tells
-        # whether to stop ot read further.
-        # 0 means keep reading; 1 means thec
-        # message is over.
-        if (i == lendata - 1):
-            if (pix[-1] % 2 == 0):
-                if(pix[-1] != 0):
-                    pix[-1] -= 1
-                else:
-                    pix[-1] += 1
-
-        else:
-            if (pix[-1] % 2 != 0):
-                pix[-1] -= 1
-                pix[-1] -= 1
-
-        pix = tuple(pix)
-        yield pix[0:3]
-        yield pix[3:6]
-        yield pix[6:9]
-
-def encode_enc(newimg, data):
-    w = newimg.size[0]
-    (x, y) = (0, 0)
-
-    for pixel in modPix(newimg.getdata(), data):
-
-        # Putting modified pixels in the new image
-        newimg.putpixel((x, y), pixel)
-        if (x == w - 1):
-            x = 0
-            y += 1
-        else:
-            x += 1
-
-# Encode data into image
-def encode_txt():
-    global dat
-    global dir1
-    global newWindow
-    img=dir1
-    image = Image.open(img, 'r')
-    data=str(dat.get())
-    if (len(data) == 0):
-        raise ValueError('Data is empty')
-
-    newimg = image.copy()
-    encode_enc(newimg, data)
-
-    new_img_name = input("Enter the name of new image(with extension) : ")
-    newimg.save(new_img_name, str(new_img_name.split(".")[1].upper()))
-
-# Decode the data in the image
-def decode_txt():
-    global dat
-    img = input("Enter image name(with extension) : ")
-    image = Image.open(img, 'r')
-
-    data = dat.get()
-    imgdata = iter(image.getdata())
-
-    while (True):
-        pixels = [value for value in imgdata.__next__()[:3] +
-                                imgdata.__next__()[:3] +
-                                imgdata.__next__()[:3]]
-
-        # string of binary data
-        binstr = ''
-
-        for i in pixels[:8]:
-            if (i % 2 == 0):
-                binstr += '0'
-            else:
-                binstr += '1'
-
-        data += chr(int(binstr, 2))
-        if (pixels[-1] % 2 != 0):
-            return data
 
 
 ###################    FRONT END     #####################################
